246f8d76bbb6e826bf34a26a1b7df083b22b8a92
Folk N <81551496+folkadonis@users.noreply.github.com>
2025-03-24 19:57:59 +0530

    Add files via upload

---
+++ fl.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from langchain_ollama import ChatOllama
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+st.title("Chatbot 🎬")
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        SystemMessage(content="Act like US president Trump")
+    ]
+for message in st.session_state.messages:
+    if isinstance(message, HumanMessage):
+        with st.chat_message("user"):
+            st.markdown(message.content)
+    elif isinstance(message, AIMessage):
+        with st.chat_message("assistant"):
+            st.markdown(message.content)
+prompt = st.chat_input("Type your message...")
+if prompt:
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    st.session_state.messages.append(HumanMessage(content=prompt))
+    llm = ChatOllama(
+        model="llama3.2:latest",  # Change model if needed
+        temperature=0.7
+    )
+    result = llm.invoke(st.session_state.messages).content
+    with st.chat_message("assistant"):
+        st.markdown(result)
+    st.session_state.messages.append(AIMessage(content=result))
